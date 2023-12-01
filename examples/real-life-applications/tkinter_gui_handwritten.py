69876e3553c6c2b17ccaaf45b69e5234ec223b0d
GitHub Action <52708150+marcpinet@users.noreply.github.com>
2023-12-01 13:01:56 +0100

    refactor: tkinter now uses real time refresh

--- examples/real-life-applications/tkinter_gui_handwritten.py
+++ examples/real-life-applications/tkinter_gui_handwritten.py
@@ -16,13 +16,13 @@ def main():
     button_clear = tk.Button(window, text="Clear", command=lambda: clear(canvas))
     button_clear.pack()
 
-    button_predict = tk.Button(window, text="Predict", command=lambda: predict(canvas))
-    button_predict.pack()
+    # Removed the Predict button as prediction is now automatic
 
     def clear(canvas):
         canvas.delete("all")
-        
+
     PEN_SIZE = 5
+
     def predict(canvas):
         img = Image.new('L', (280, 280), color='black')
         draw = ImageDraw.Draw(img)
@@ -30,9 +30,9 @@ def main():
         for item in canvas.find_all():
             coords = canvas.coords(item)
             for i in range(0, len(coords), 2):
-                x, y = coords[i], coords[i+1]
-                draw.ellipse((x-PEN_SIZE, y-PEN_SIZE, x+PEN_SIZE, y+PEN_SIZE), fill='white')
-                
+                x, y = coords[i], coords[i + 1]
+                draw.ellipse((x - PEN_SIZE, y - PEN_SIZE, x + PEN_SIZE, y + PEN_SIZE), fill='white')
+
         img = img.resize((28, 28))
         img = img.convert('L')
         img = np.array(img)
@@ -41,16 +41,17 @@ def main():
         prediction = model.predict(img)
         prediction = np.argmax(prediction)
         label.configure(text="Prediction: " + str(prediction))
-        
+
     def paint(event):
         x1, y1 = (event.x - PEN_SIZE), (event.y - PEN_SIZE)
         x2, y2 = (event.x + PEN_SIZE), (event.y + PEN_SIZE)
         canvas.create_oval(x1, y1, x2, y2, fill="white", outline="white")
-        
+        predict(canvas)  # Call predict function here for real-time prediction
+
     canvas.bind("<B1-Motion>", paint)
-        
+
     window.mainloop()
-    
+
 
 if __name__ == '__main__':
     main()
