78cd82b07a06f192a329f93d5531aeb560705df4
GitHub Action <52708150+marcpinet@users.noreply.github.com>
2025-08-07 22:23:38 +0200

    ci: bump version to 4.3.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neuralnetlib',
-    version='4.3.4',
+    version='4.3.5',
     author='Marc Pinet',
     description='A flexible deep learning framework built from scratch using only NumPy',
     long_description=open('README.md', encoding="utf-8").read(),
